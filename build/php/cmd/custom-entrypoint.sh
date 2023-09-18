58b853af3f6264d0c22c97a7bd8a32f8e73507c9
saudisimon <kennyssq1309@gmail.com>
2023-09-18 18:14:30 +0700

    Init magento cloud docker multiple php

---
+++ build/php/cmd/custom-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Refer: /usr/local/bin/docker-php-entrypoint
+set -e
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+    set -- php-fpm "$@"
+fi
+
+# Testing sendmail
+php -r "\$from = \$to = 'php-contaier@docker.local'; \$x = mail(\$to, 'Testing email on startup', 'Hello World', 'From: '. \$from); var_dump(\$x);"
+
+# Fix permission
+chown nginx:nginx /home/nginx/.composer
+chmod 644 /home/nginx/.ssh/config \
+&& chmod -R 600 /home/nginx/.ssh/* \
+&& chmod 755 /home/nginx/.ssh/nestle \
+&& chown -R nginx:nginx /home/nginx/ \
+&& eval `ssh-agent -s`
+
+exec "$@"
